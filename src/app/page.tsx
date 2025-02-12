96b466081e1d7295217ecf4731e0fa629ad2854a
Shivam Bisht <rajshivam2745@gmail.com>
2025-02-13 01:14:04 +0530

    courses section add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <FeaturedCourses />
     </>
   );
 }
